d1f855ea033d34552320292da7483215d8801ff7
fernando <fernandodat600@gmail.com>
2025-02-24 18:45:30 -0300

    Criando o valor da despesa

--- script.js
+++ script.js
@@ -60,23 +60,29 @@ function expenseAdd(newExpense) {
         expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
         expenseIcon.setAttribute("alt", newExpense.category_name)
 
-      // Cria a info da despesa
-      const expenseInfo = document.createElement("div")
-      expenseInfo.classList.add("expense-info")
+        // Cria a info da despesa
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
 
-      //Cria o nome da despesa
-      const expenseName = document.createElement("strong")
-      expenseName.textContent = newExpense.expense
+        // Cria o nome da despesa
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
 
-      //Cria a categoria da despesa
-      const expenseCategory = document.createElement("span")
-      expenseCategory.textContent = newExpense.category_name
+        // Cria a categoria da despesa
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
 
-      //Adiciona name e category em expenseInfo
-      expenseInfo.append(expenseName, expenseCategory)
+        // Adiciona o nome e a categoria na div das informações da despesa
+        expenseInfo.append(expenseName, expenseCategory)
+
+        // Criando o valor da despesa
+        const expenseAmount = document.createElement("span")
+        expenseAmount.classList.add("expense-amount")
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
+
 
         // Adiciona as informações no item
-        expenseItem.append(expenseIcon, expenseInfo)
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
 
         // Adiciona o item na lista
         expenseList.append(expenseItem)
